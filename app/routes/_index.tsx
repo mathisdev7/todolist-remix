8b687a4daa0b2f58831e2da0c166c3cb4fa24c6a
Frost <mathis@macbook-pro-de-mathis.home>
2024-09-09 00:05:24 +0200

    edited index page and added font

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -32,10 +32,15 @@ export const loader = async ({ request }: { request: Request }) => {
 export default function Index() {
   const data: LoaderData = useLoaderData();
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl font-bold">
-        Welcome to Remix, {data.user.name}!
+    <div className="flex min-h-full flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 gap-6 relative top-8">
+      <h1 className="text-3xl font-bold text-center">
+        Welcome back,
+        <span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#b897d9] to-[#581d93]">
+          {" "}
+          {data.user.name}.
+        </span>
       </h1>
+      <p className="text-center font-sans">There are no todos yet.</p>
     </div>
   );
 }
