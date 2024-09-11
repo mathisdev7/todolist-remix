2a443f438bb1980140aaf9b1c3a3e84f6b22dca9
Frost <mathis@mbp-de-mathis-1.home>
2024-09-12 01:07:39 +0200

    vercel build not working 5

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,9 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
   ignoredRouteFiles: ["**/.*"],
-  server: "./build/index.js",
-  future: {
-    v2_routeConvention: true, // Si tu utilises les conventions de Remix v2
-  },
 };
